ce8849e8c8966f84fcd0a3b7eac5e2fa34dc7bf7
Patrick Albuquerque <patrickalbuq@gmail.com>
2024-08-30 12:50:06 -0300

    [add] Rota para criar produto adicionada

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/product-info', Products.getProductInfo)
 router.get('/products-info', Products.getAllProductsInfo)
-// router.post('/create-product', Products.addNewProduct) TO:DO
+router.post('/create-product', Products.addNewProduct)
 // router.post('/update-product', Products.updateProduct) TO:DO
 // router.post('/change-product-quantity', Products.changeProductQuantity) TO:DO
 // router.delete('/delete-product', Products.deleteProduct) TO:DO
